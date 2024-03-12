6bf0e012194b21c7c6f3dd89a6b856e66275dfce
kajanav <vkajana@std.appsc.sab.ac.lk>
2024-03-12 22:24:32 +0530

    Index page added

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Hero } from '../Components/Hero/Hero'
+import { Popular } from '../Components/Popular/Popular'
+import { Offers } from '../Components/Offers/Offers'
 
 export const Shop = () => {
   return (
-    <div>Shop</div>
+    <div>
+      <Hero/>
+      <Popular/>
+      <Offers/>
+      
+    </div>
   )
 }
